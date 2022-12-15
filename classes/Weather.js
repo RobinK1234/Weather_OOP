f60d0766c2be275430607a83d48faf1ddde67e12
Robin <robin.kriisa@voco.ee>
2022-12-15 11:40:36 +0200

    OOP weather!!

--- classes/Weather.js
+++ classes/Weather.js
@@ -4,9 +4,9 @@ class Weather{
         this.city = city
     }
     async getWeather(){
-        const responce = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`)
-        const responceData = await responce.json()
-        return responceData
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`)
+        const responseData = await response.json()
+        return responseData
     }
     changeCity(city){
         this.city = city
